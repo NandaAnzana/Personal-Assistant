63f5dc3741aabbd908c6412bb4128f853224e9f8
NandaAnzana <nanda.anzana@gmail.com>
2020-08-03 22:51:40 +0700

    add MHA.py which is Multi Head Attention layer from tranformers

--- src/model.py
+++ src/model.py
@@ -1,8 +1,3 @@
-import transformers
-from tensorflow.keras.layers import Embedding, Dense, Bidirectional, LSTM
-from tensorflow.keras.models import Sequential
-import tensorflow_datasets as tfds
 import tensorflow as tf
-from sklearn.preprocessing import OneHotEncoder
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from tensorflow.keras.layers import LeakyReLU
+import numpy as np
+import math
